e27c91275ca710a05ad96110ca102b2dad656b27
muhammad.ahmad2 <muhammad.ahmad@narayanagroup.com>
2023-06-08 18:12:30 +0530

    After Pipeline 4

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -44,7 +44,7 @@ class MealsScreen extends StatelessWidget {
           children: const [
             Text('After PIPELINE'),
             SizedBox(height: 16),
-            Text('3'),
+            Text('4'),
           ],
         ),
       );
