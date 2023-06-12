97281eefdd0a4c554c7fcda5f86b6f61834c6e8d
muhammad.ahmad2 <muhammad.ahmad@narayanagroup.com>
2023-06-12 11:36:58 +0530

    Netlify_Build_Test_9

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -44,7 +44,7 @@ class MealsScreen extends StatelessWidget {
           children: const [
             Text('After PIPELINE'),
             SizedBox(height: 16),
-            Text('Netlify 8'),
+            Text('Netlify 9'),
           ],
         ),
       );
